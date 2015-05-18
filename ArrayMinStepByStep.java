3a51a0714f151c5e2952d03e93aa2de28c4b65e7
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2015-05-18 11:46:36 +0200

    Updated

--- ArrayMinStepByStep.java
+++ ArrayMinStepByStep.java
@@ -7,7 +7,7 @@ class ArrayMin {
         for (int j = 0; j < SIZE; ++j) {
             @3@8array[j] = unknownInt();@@           (*\only<3-11>{[{\color{darkpurple}\bfseries\only<3-7>{{\btHL{0}}}\only<8-11>{0}\only<6-7>{, -1}\only<8-11>{, {\btHL{-1}}}}]}*)
 
-            if (@4@9array[j] < min@@) {
+            if (@4@9array[j] < min@@) {              (*\only<4>{{\color{darkblue}\bfseries{nondet}}}\only<5>{{\color{darkorange}\bfseries{0 < 0}}}\only<10>{{\color{darkorange}\bfseries{-1 < 0}}}*)
                 @10min = array[j];@                (*\only<5>{{\color{darkorange}\bfseries{pruned}}, {\color{darkblue}\bfseries{feasible}}}*)
             }
         }
---
+++ TraceFormula.java
@@ -0,0 +1,9 @@
+(*\color{darkorange}\bfseries\{*)
+  (*\def\kw#1{{\color{darkblue}\bfseries#1}}\btHL<5>{cfg[0] = \btHL<2>{\kw{new} Node()}}*);
+
+  @6assert cfg[0].length > 0@;
+(*\color{darkorange}\bfseries\}*)
+
+Node() (*\color{darkblue}\bfseries\{*)
+  (*\def\kw#1{{\color{darkblue}\bfseries#1}}\btHL<4>{\kw{this}.facts = \btHL<3>{\kw{new} \kw{int}[1]}}*);
+(*\color{darkblue}\bfseries\}*)
