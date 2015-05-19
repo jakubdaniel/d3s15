ca59ba97ded0342834519092fcdc8519a3d09df8
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2015-05-19 11:18:57 +0200

    Fix

--- TraceFormula.java
+++ TraceFormula.java
@@ -1,7 +1,7 @@
 (*\color{darkorange}\bfseries\{*)
   (*\def\kw#1{{\color{darkblue}\bfseries#1}}\btHL<5>{cfg[0] = \btHL<2>{\kw{new} Node()}}*);
 
-  @6assert cfg[0].length > 0@;
+  @6assert cfg[0].facts.length > 0@;
 (*\color{darkorange}\bfseries\}*)
 
 Node() (*\color{darkblue}\bfseries\{*)
